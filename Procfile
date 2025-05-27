68cbce3f3812abd5e7b3f646bcf6a74fd6375b66
teaghan <obriaintb@gmail.com>
2025-05-26 17:35:34 -0700

    teacher authentication

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python startup.py & wait
-web: python utils/deployment/secrets.py && streamlit run app.py --server.port $PORT --server.headless true & python warm_start.py & wait
+web: streamlit run app.py --server.port $PORT --server.headless true & python warm_start.py & python utils/deployment/secrets.py & wait
--- utils/deployment/secrets.py
+++ utils/deployment/secrets.py
@@ -17,22 +17,22 @@ def write_secrets_from_env():
     with secrets_path.open("w") as f:
         f.write("[auth]\n")
         f.write(f'redirect_uri = "{domain_url}/oauth2callback"\n')
-        f.write(f'cookie_secret = "{os.getenv("STREAMLIT_AUTH_COOKIE_SECRET", "")}"\n\n')
+        f.write(f'cookie_secret = "{os.getenv("STREAMLIT_AUTH_COOKIE_SECRET")}"\n\n')
 
         f.write("[auth.google]\n")
-        f.write(f'client_id = "{os.getenv("STREAMLIT_AUTH_GOOGLE_CLIENT_ID", "")}"\n')
-        f.write(f'client_secret = "{os.getenv("STREAMLIT_AUTH_GOOGLE_CLIENT_SECRET", "")}"\n')
+        f.write(f'client_id = "{os.getenv("STREAMLIT_AUTH_GOOGLE_CLIENT_ID")}"\n')
+        f.write(f'client_secret = "{os.getenv("STREAMLIT_AUTH_GOOGLE_CLIENT_SECRET")}"\n')
         f.write('server_metadata_url = "https://accounts.google.com/.well-known/openid-configuration"\n\n')
 
         f.write("[auth.microsoft]\n")
-        f.write(f'client_id = "{os.getenv("STREAMLIT_AUTH_MICROSOFT_CLIENT_ID", "")}"\n')
-        f.write(f'client_secret = "{os.getenv("STREAMLIT_AUTH_MICROSOFT_CLIENT_SECRET", "")}"\n')
+        f.write(f'client_id = "{os.getenv("STREAMLIT_AUTH_MICROSOFT_CLIENT_ID")}"\n')
+        f.write(f'client_secret = "{os.getenv("STREAMLIT_AUTH_MICROSOFT_CLIENT_SECRET")}"\n')
         f.write('server_metadata_url = "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration"\n\n')
 
         f.write("[auth.auth0]\n")
-        f.write(f'client_id = "{os.getenv("STREAMLIT_AUTH_AUTH0_CLIENT_ID", "")}"\n')
-        f.write(f'client_secret = "{os.getenv("STREAMLIT_AUTH_AUTH0_CLIENT_SECRET", "")}"\n')
-        f.write(f'server_metadata_url = "{os.getenv("STREAMLIT_AUTH_AUTH0_METADATA_URL", "")}"\n')
+        f.write(f'client_id = "{os.getenv("STREAMLIT_AUTH_AUTH0_CLIENT_ID")}"\n')
+        f.write(f'client_secret = "{os.getenv("STREAMLIT_AUTH_AUTH0_CLIENT_SECRET")}"\n')
+        f.write(f'server_metadata_url = "{os.getenv("STREAMLIT_AUTH_AUTH0_METADATA_URL")}"\n')
         f.write('client_kwargs = { prompt = "login" }\n')
 
     logger.info(f"Secrets written to {secrets_path}")
